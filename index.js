1056a02346def899f057c8b669d0506e6213c0c7
baigsufian6 <baigsufian6@gmail.com>
2024-05-16 11:37:06 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+
+app.get('/sufi', (req, res) => {
+    res.send('<h1>is good!!</h1>')
+  })
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
